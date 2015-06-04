d0103cfab5fe9b6c787189b7fc815401e2ed378c
Brandon Hilkert <brandonhilkert@gmail.com>
2015-06-04 09:16:08 -0400

    Use puma

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bin/unicorn -p $PORT -c ./config/unicorn.rb
+web: bin/puma -t 0:5 -p $PORT -e $RACK_ENV
 
--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'bundler'
 Bundler.require
+require "sinatra/reloader" if development?
+
+set :static_cache_control, [:public, max_age: 60 * 60 * 24 * 365]
 
 get '/' do
   erb :index
